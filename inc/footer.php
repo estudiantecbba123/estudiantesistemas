061c446d9ea3e8c1d9f2f54bdb5676f8ab3bd210
angelAdrian <abc@gmail.com>
2022-10-27 13:57:14 -0400

    Este es la primera version del proyecto

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+
+  <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.js"></script>            
+  <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.bundle.min.js"></script> 
+
+</body>
+</html>
